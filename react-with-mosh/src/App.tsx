0f0c79697bbe2ab85b342ecedded56a91d2dc658
rajeev2834 <rajeev2834@gmail.com>
2023-05-04 09:16:20 +0530

    Materil UI started

--- react-with-mosh/src/App.tsx
+++ react-with-mosh/src/App.tsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Divider from '@mui/material/Divider';
+import { Button, Color, TextField } from '@mui/material';
+
+
 interface User{
     id: number;
     name: string;
@@ -29,15 +38,68 @@ function App(){
         return () => controller.abort();
     }, []);
 
+    const deleteUser = (id: number) => {
+        const originalUsers = [...users];
+        setUsers(users.filter((user) => user.id !== id));
+        axios.delete(`https://jsonplaceholder.typicode.com/xusers/${id}`)
+            .catch((err) => {
+                setError(err.message);
+                setUsers(originalUsers);
+            })
+    }
+
+    const addUser = () => {
+        const originalUsers = [...users];
+        const newUser = {
+            id: users.length + 1,
+            name: 'Rajeev',
+        };
+        setUsers([...users, newUser]);
+        axios.post('https://jsonplaceholder.typicode.com/xusers', newUser)
+           .then((res) => {
+               setUsers([...users, res.data]);
+           })
+           .catch((err) => {
+               setError(err.message);
+               setUsers(originalUsers);
+           })
+    }
+
+    const updateUsers = (user : User) => {
+        const originalUsers = [...users];
+        //update the user
+        const updatedUser = {...user, name: 'Rajeev'};
+
+        setUsers(users.map((u) => u.id === user.id ? updatedUser : u));
+
+        axios.patch(`https://jsonplaceholder.typicode.com/xusers/${user.id}`, updatedUser)
+        .catch((err) => {
+            setError(err.message);
+            setUsers(originalUsers);
+        });
+    }
+
     return (
         <>
          {isLoading && <p>Loading...</p>}
-         {error &&  <p className='text-danger'>{error}</p>}
+         {error &&  <p style={{color: 'red'}}>{error}</p>}
         <div>
-            <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
-            </ul>
-        </div>
+        <Button variant="contained" color='primary' onClick={() => addUser()}>Add User</Button>
+        <List>
+        {users.map((user, index) => (
+        <React.Fragment key={user.id}>
+        <ListItem>
+            <ListItemText primary={user.name} />
+            <Button variant="outlined" color='secondary' onClick={() => updateUsers(user)}>Update</Button>
+            <IconButton edge="end" color="error" onClick={() => deleteUser(user.id)}>
+            <DeleteIcon />
+            </IconButton>
+        </ListItem>
+        {index !== users.length - 1 && <Divider />}
+        </React.Fragment>
+        ))}
+        </List>
+            </div>
         </>
        
     )
